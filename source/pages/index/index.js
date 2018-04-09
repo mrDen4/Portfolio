0cf0c4c7697b1b3c68c31b70278f619db04527e6
Denis Davydov <den.saint.k@gmail.com>
2018-04-10 01:33:22 +0300

    add svg in index.html

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,2 +1,5 @@
 import 'normalize.css';
 import './scss/index.scss';
+import '../../assets/svg/to_sprite/vk.svg';
+import '../../assets/svg/to_sprite/github.svg';
+import '../../assets/svg/to_sprite/in.svg';
